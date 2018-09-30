5d598a58ca3df1635d3c7df33945afd462aaa25a
Kai Le Wang <s3543953@student.rmit.edu.au>
2018-09-30 07:00:35 +0000

    file setup

---
+++ main.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as GPIO
+import time
+import Adafruit_CharLCD as LCD
+import sys
+import os
+
+GPIO.setmode(GPIO.BCM) # set board mode to Broadcom
+##LED SETUP
+GPIO.setup(3,GPIO.OUT,initial=GPIO.LOW) # set up pin 18
+GPIO.setup(4,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(4,GPIO.RISING)
+buttonStat=False
+
+def my_gpioCallback(channel):
+    global buttonStat
+    start_time = time.time()
+    buttonTime=0
+    while GPIO.input(channel):
+        if (buttonTime<=2):
+            buttonTime=time.time()-start_time
+        else:
+            print(buttonTime)
+            buttonStat=not(buttonStat and buttonStat)
+            return;
+
+
+def service(status):
+    if status=='ON':
+        print('ON')
+        GPIO.output(3,1)
+        os.system('sudo ifup usb0')
+        os.system('sudo ifconfig wlan0 up')
+    elif status=='OFF':
+        print('OFF')
+        GPIO.output(3,0)
+        os.system('sudo ifdown usb0')
+        os.system('sudo ifconfig wlan0 down')
+    
+
+if __name__=="__main__":
+    GPIO.add_event_callback(4,my_gpioCallback)
+    temp = buttonStat
+    while True:    
+        if(temp!=buttonStat):
+            if buttonStat==False:
+                buttonStat=False
+                service('OFF')
+            else:
+                buttonStat=True
+                service('ON')
+            temp=buttonStat
+
+
+
